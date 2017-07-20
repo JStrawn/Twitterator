adbcf05ada534872fef254e72b7869d1c4c95275
Louis <Lharris134@gmail.com>
2017-07-20 13:21:08 -0400

    created viewcontrollers

--- Twitterater/Twitterater/AppDelegate.swift
+++ Twitterater/Twitterater/AppDelegate.swift
@@ -17,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let rootController = SearchScreenViewController()
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        rootController.view.frame = self.window!.frame
+        self.window!.rootViewController = rootController
+        self.window!.makeKeyAndVisible()
+        
         return true
     }
 
